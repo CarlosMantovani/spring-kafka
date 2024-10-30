07bb9238b1c77b46667448504146163dd802f790
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-10-30 15:01:59 -0300

    feature:criando controller para postagem do carro e do vendedor

---
+++ api/src/main/java/mantovani/dev/api/controller/CarPostController.java
@@ -0,0 +1,37 @@
+package mantovani.dev.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import mantovani.dev.api.dto.CarPostDTO;
+import mantovani.dev.api.service.CarPostStoreService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/car")
+@RequiredArgsConstructor
+public class CarPostController {
+
+    private CarPostStoreService carPostStoreService;
+
+
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<CarPostDTO>> getCarSales(){
+        return ResponseEntity.status(HttpStatus.FOUND).body(carPostStoreService.getCarForSales());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity changeCarSale(@RequestBody CarPostDTO carPostDTO, @PathVariable("id") String id){
+        carPostStoreService.changeCarForSale(carPostDTO,id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCarForSale(@PathVariable("id") String id){
+        carPostStoreService.removeCarForSale(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
