7cb8929b224cc9dcb9227df167221c0f68986c75
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-01 17:11:19 -0300

    refactor: adicionando @Component

--- car/src/main/java/mantovani/dev/car/dto/mapper/OwnerMapper.java
+++ car/src/main/java/mantovani/dev/car/dto/mapper/OwnerMapper.java
@@ -4,7 +4,9 @@ package mantovani.dev.car.dto.mapper;
 import mantovani.dev.car.dto.OwnerPostDTO;
 import mantovani.dev.car.entity.OwnerPostEntity;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OwnerMapper {
     public OwnerPostEntity toOwnerPostDto(OwnerPostDTO ownerPostDTO){
         return new ModelMapper().map(ownerPostDTO, OwnerPostEntity.class);
