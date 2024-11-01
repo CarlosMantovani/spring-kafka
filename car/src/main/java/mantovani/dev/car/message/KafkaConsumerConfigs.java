3466278d6eefd7ed51865ede4dce73f8520951f9
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-01 17:08:48 -0300

    refactor: criando uma variavel para fazer a deserialização

--- car/src/main/java/mantovani/dev/car/message/KafkaConsumerConfigs.java
+++ car/src/main/java/mantovani/dev/car/message/KafkaConsumerConfigs.java
@@ -1,9 +1,8 @@
 package mantovani.dev.car.message;
 
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import mantovani.dev.car.dto.CarPostDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,20 +25,20 @@ public class KafkaConsumerConfigs {
     public ConsumerFactory<String, CarPostDTO> consumerFactory() {
 
         Map<String, Object> props = new HashMap<>();
-
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "store-posts-group");
-        props.put(JsonDeserializer.TRUSTED_PACKAGES,"*");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props, (Deserializer) new StringDeserializer(),
-                new JsonDeserializer<>(CarPostDTO.class, false));
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        JsonDeserializer<CarPostDTO> jsonDeserializer = new JsonDeserializer<>(CarPostDTO.class, false);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), jsonDeserializer);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, CarPostDTO> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, CarPostDTO>
-                factory = new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, CarPostDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
