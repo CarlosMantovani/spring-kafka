c5a5c166d10996ff3febf6469fab7510f8eeebfb
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-04 17:11:16 -0300

    refactor: criando metodos de busca no banco

--- data/src/main/java/mantovani/dev/data/Service/PostAnalyticsService.java
+++ data/src/main/java/mantovani/dev/data/Service/PostAnalyticsService.java
@@ -1,6 +1,10 @@
 package mantovani.dev.data.Service;
 
 import lombok.RequiredArgsConstructor;
+import mantovani.dev.data.controller.mapper.AnalyticsMapper;
+import mantovani.dev.data.dto.AnalyticsBrandDTO;
+import mantovani.dev.data.dto.AnalyticsModelDTO;
+import mantovani.dev.data.dto.AnalyticsPriceDTO;
 import mantovani.dev.data.dto.CarPostDTO;
 import mantovani.dev.data.entity.BrandAnalyticsEntity;
 import mantovani.dev.data.entity.CarModelAnalyticsEntity;
@@ -10,6 +14,11 @@ import mantovani.dev.data.repository.CarModelAnalyticsRepository;
 import mantovani.dev.data.repository.CarPriceAnalyticsRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class PostAnalyticsService {
@@ -18,11 +27,29 @@ public class PostAnalyticsService {
     private final CarModelAnalyticsRepository carModelAnalyticsRepository;
     private final CarPriceAnalyticsRepository carPriceAnalyticsRepository;
 
+    private final AnalyticsMapper analyticsMapper;
     public void saveDataAnalytics(CarPostDTO carPostDTO){
         saveBrandAnalytics(carPostDTO.getBrand());
         saveCarModelAnalytics(carPostDTO.getModel());
         saveCarModelPriceAnalytics(carPostDTO.getModel(), carPostDTO.getPrice());
     }
+
+    public List<AnalyticsBrandDTO> getAnalyticsBrand(){
+        List<AnalyticsBrandDTO> analyticsBrandList = new ArrayList<>();
+        return brandAnalyticsRepository.findAll().stream().map(analyticsMapper::toAnalyticsBrandDTO)
+                .collect(Collectors.toList());
+    }
+    public List<AnalyticsModelDTO> getAnalyticsModel(){
+        List<AnalyticsModelDTO> analyticsModelList = new ArrayList<>();
+        return carModelAnalyticsRepository.findAll().stream().map(analyticsMapper::toAnalyticsModelDTO)
+                .collect(Collectors.toList());
+    }
+    public List<AnalyticsPriceDTO> getAnalyticsPrice(){
+        List<AnalyticsPriceDTO> analyticsPriceList = new ArrayList<>();
+        return carPriceAnalyticsRepository.findAll().stream().map(analyticsMapper::toAnalyticsPriceDTO)
+                .collect(Collectors.toList());
+    }
+
     private void saveBrandAnalytics(String brand){
 
         BrandAnalyticsEntity brandAnalyticsEntity = new BrandAnalyticsEntity();
