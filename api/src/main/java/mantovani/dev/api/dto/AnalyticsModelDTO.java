8b147d15c26708786585430ce93abf9acc44d355
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-04 17:12:13 -0300

    feature: Criando Dto para manipulação das informações vinda do banco

---
+++ api/src/main/java/mantovani/dev/api/dto/AnalyticsModelDTO.java
@@ -0,0 +1,17 @@
+package mantovani.dev.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude
+public class AnalyticsModelDTO {
+    private String model;
+    private Long posts;
+}
---
+++ api/src/main/java/mantovani/dev/api/dto/AnalyticsPriceDTO.java
@@ -0,0 +1,18 @@
+package mantovani.dev.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude
+public class AnalyticsPriceDTO {
+
+    private  String model;
+    private double price;
+}
